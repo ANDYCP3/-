6816767dd74820f95b58b55a20f4a6558dd2fc4c
ANDYCP3 <1084024616@qq.com>
2025-09-08 11:24:03 +0800

    Delete H5格式以及读取代码/海鹰 directory

--- H5格式以及读取代码/海鹰/ceshi.py
+++
@@ -1,6 +0,0 @@
-met_data = read_met_file("20230723.met")
-
-for i, record in enumerate(met_data[:5], 1):
-    print(f"Record {i} | ERROR: {record['ERROR']} | VELOCITY: {record['VELOCITY']}")
-    print(f"  SNR: {record['SNR']}")
-    print(f"  POWER: {record['POWER']}")
